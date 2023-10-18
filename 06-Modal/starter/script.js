b6f7eca0759d34661568ce5d85a574c6176a3630
IVAN TORRES MARCOS <ivatormar2@alu.edu.gva.es>
2023-10-18 09:33:12 +0200

    80.

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,23 @@
 'use strict';
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+
+// const btnsOpenModal=document.querySelector('.show-modal')//Si hiciesemos esto al ahber varios solo cogería el primero de todos, por lo tanto tenemos que hacer un .querySelectorAll
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', openModal);
+}
+btnCloseModal.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
