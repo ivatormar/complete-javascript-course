af770d4d7534f40f3be6b5b254c20c78905f8136
IVAN TORRES MARCOS <ivatormar2@alu.edu.gva.es>
2023-10-18 15:37:17 +0200

    83 5:31min

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,1 +1,29 @@
 'use strict';
+
+//Selecting elements
+const score0Element=document.querySelector('#score--0')
+const score1Element =document.getElementById('score--1')
+const diceElement=document.querySelector('.dice')
+const btnNew=document.querySelector('.btn--new')
+const btnRoll=document.querySelector('.btn--roll')
+const btnHold=document.querySelector('.btn--hold')
+
+
+
+
+//Starting conditions
+score0Element.textContent=0;
+score1Element.textContent=0;
+diceElement.classList.add('hidden ')
+
+
+//Rolling dice functionality
+btnRoll.addEventListener('click',function(){
+    //1.Generating arandom dice roll
+
+
+    //2. Display the dice
+
+
+    //3. Check for if a rolled 1: if ture, switch player
+})
