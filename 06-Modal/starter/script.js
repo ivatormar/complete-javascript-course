68eb8f17b6ec8fbdac09576fed07aa3b7677d4ac
IVAN TORRES MARCOS <ivatormar2@alu.edu.gva.es>
2023-10-18 14:59:26 +0200

    81

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -18,6 +18,13 @@ const openModal = function () {
 for (let i = 0; i < btnsOpenModal.length; i++) {
   btnsOpenModal[i].addEventListener('click', openModal); //
 }
-btnCloseModal.addEventListener('click', closeModal);
 
+
+btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown',function(eevent){
+    console.log(eevent);
+    if(eevent.key==='Escape')closeModal()
+     
+})
