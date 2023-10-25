46b4d3634e328aa6aaac885e1ea19a8632a276c8
IVAN TORRES MARCOS <ivatormar2@alu.edu.gva.es>
2023-10-25 22:13:33 +0200

    83 acabado

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,29 +1,36 @@
 'use strict';
 
 //Selecting elements
-const score0Element=document.querySelector('#score--0')
-const score1Element =document.getElementById('score--1')
-const diceElement=document.querySelector('.dice')
-const btnNew=document.querySelector('.btn--new')
-const btnRoll=document.querySelector('.btn--roll')
-const btnHold=document.querySelector('.btn--hold')
+const score0Element = document.querySelector('#score--0');
+const score1Element = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
 
-
-
+const diceElement = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
 
 //Starting conditions
-score0Element.textContent=0;
-score1Element.textContent=0;
-diceElement.classList.add('hidden ')
-
+score0Element.textContent = 0;
+score1Element.textContent = 0;
+diceElement.classList.add('hidden ');
+let currentScore = 0;
 
 //Rolling dice functionality
-btnRoll.addEventListener('click',function(){
-    //1.Generating arandom dice roll
+btnRoll.addEventListener('click', function () {
+  //1.Generating arandom dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
 
-
-    //2. Display the dice
-
-
-    //3. Check for if a rolled 1: if ture, switch player
-})
+  //2. Display the dice
+  diceElement.classList.remove('hidden'); //*Lo hacemos visible
+  diceElement.src = `dice-${dice}.png`;
+  //3. Check for if a rolled 1: if ture,
+  if (dice === 1) {
+    //switch player
+  } else {
+    //Add doce tp tje cirremt scpre
+    currentScore += dice;
+    current0El.textContent = currentScore;
+  }
+});
